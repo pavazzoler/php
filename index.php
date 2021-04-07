2683764aeb04ba14fc45b0939c1984498a8c6747
pavazzoler <82113204+pavazzoler@users.noreply.github.com>
2021-04-07 14:27:33 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "172.31.15.246";
+$database = "adtsys";
+$username = "root";
+$password = "pa250285";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $database);
+// Check connection
+if (!$conn) {
+    die("Nao Conectado com o Banco de Dados!" . mysqli_connect_error());
+}
+echo "Conectado com o Banco de Dados!";
+mysqli_close($conn);
+?>
